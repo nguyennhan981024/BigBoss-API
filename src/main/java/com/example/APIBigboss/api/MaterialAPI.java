fead60324cdffc54e0597366ecdf24c8fd0e465c
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2021-09-10 23:23:43 +0700

    vanh update api

--- src/main/java/com/example/APIBigboss/api/MaterialAPI.java
+++ src/main/java/com/example/APIBigboss/api/MaterialAPI.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/material")
 public class MaterialAPI {
+    
     @Autowired
     private MaterialRepository materialRepository;
 
